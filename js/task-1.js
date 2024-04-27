d20444f716d53ff7d0398d86aa66f228d3ed7806
Svitlana Ruts <160260280+svitlana-ruts@users.noreply.github.com>
2024-04-27 23:55:04 +0200

    adds task3

--- js/task-1.js
+++ js/task-1.js
@@ -7,3 +7,24 @@ function makeTransaction (quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+//Задача 2. Доставка товару
+
+function getShippingMessage(country, price, deliveryFee) {
+    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+};
+
+console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+//Задача 3. Ширина елемента
+
+function getElementWidth(content, padding, border) {
+    return content + (padding * border);
+};
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+//Задача 3. Ширина елемента
+
+function getElementWidth(content, padding, border) {
+    return content + (padding * border);
+};
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
